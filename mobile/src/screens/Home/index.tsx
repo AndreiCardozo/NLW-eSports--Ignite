a385f1c69b91a454e3bc4e41c7f8bae0a059491e
Cardozo <andreicardozo300@gmail.com>
2022-10-03 17:02:07 -0300

    Aplicação Mobile

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,44 @@
+import { View, Image, FlatList } from 'react-native';
+import { useEffect, useState } from 'react';
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+import { styles } from './styles';
+
+export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+  useEffect(() => {
+    fetch('http://172.29.128.1/games')
+      .then(response => response.json())
+      .then(data => setGames(data));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+
+      <Heading
+        title="Encontre seu duo"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+
+      <FlatList
+        data={games}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <GameCard
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        horizontal
+        contentContainerStyle={styles.contentList}
+      />
+
+    </View>
+  );
+}
